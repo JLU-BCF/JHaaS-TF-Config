0cddc923180a3ce9b0bb2239c10a75dbaee0e316
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2024-07-16 13:04:38 +0200

    Update provider versions

--- providers.tf
+++ providers.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     authentik = {
       source  = "goauthentik/authentik"
-      version = "~> 2023.10.0"
+      version = "~> 2024.6.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.12.0"
+      version = "~> 2.14.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.24.0"
+      version = "~> 2.31.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = "1.13.0"
+      version = "1.14.0"
     }
   }
 }
